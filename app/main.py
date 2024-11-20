e04c8d978e7c0eb46c282f2f567f05632651e68a
Prawaan <prawaan.singh@thoughtspot.com>
2024-11-20 15:10:27 +0530

    added multiple client support

--- app/main.py
+++ app/main.py
@@ -1,4 +1,15 @@
 import socket  # noqa: F401
+import threading as t
+
+
+def handle_request(connection, request_num):
+    print(f"Handling request {request_num}")
+    while True:
+        data = connection.recv(1024)
+        if not data:
+            break
+        connection.sendall(b"+PONG\r\n")
+    print(f"Request handled : {request_num}")
 
 
 def main():
@@ -8,14 +19,14 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept()  # wait for client
+    client_num = 0
     while True:
-        data = connection.recv(1024)
-        if not data:
-            break
-        connection.sendall(b"+PONG\r\n")
-    connection.close()
+        connection, _ = server_socket.accept()
+        client_num += 1
+        thread1 = t.Thread(target=handle_request, args=(connection, client_num))
+        thread1.start()
 
+    # server_socket.close()
 
 if __name__ == "__main__":
     main()

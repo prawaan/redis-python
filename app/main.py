110ccba074183d439a75a3dfea1b46e1a8e767ca
Prawaan <prawaan.singh@thoughtspot.com>
2024-11-21 10:20:18 +0530

    added support for config command

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
 import socket  # noqa: F401
 import threading as t
 import time
+import argparse
 
 store: dict = {}
+config: dict = {}
+
 
 def wrap(data):
+    if isinstance(data, list):
+        return f"*{len(data)}\r\n" + "".join([wrap(d) for d in data])
     if data == "nil":
         return "$-1\r\n"
     return f"${len(data)}\r\n{data}\r\n"
+
 
 def current_milli_time():
     return round(time.time() * 1000)
@@ -16,27 +22,31 @@ def current_milli_time():
 def process_data(data):
     cmd = data[2]
     print(f"command: {cmd}")
-    if cmd == 'echo':
+    if cmd == "echo":
         return data[4]
-    if cmd == 'ping':
+    if cmd == "ping":
         return "PONG"
-    if cmd == 'set':
-        if len(data) > 8 and data[8] == 'px':
+    if cmd == "set":
+        if len(data) > 8 and data[8] == "px":
             store[data[4]] = (data[6], current_milli_time() + int(data[10]))
         else:
             store[data[4]] = (data[6], -1)
         return "OK"
-    if cmd == 'get':
+    if cmd == "get":
         value = store.get(data[4], "nil")
-        if value != 'nil' and value[1] != -1 and value[1] < current_milli_time():
+        if value != "nil" and value[1] != -1 and value[1] < current_milli_time():
             store.pop(data[4])
             return "nil"
         return value[0]
-    
+    if cmd == "config" and data[4] == "get":
+        if data[6] == "dbfilename":
+            return ["dbfilename", config.get("dbfilename", "nil")]
+        if data[6] == "dir":
+            return ["dir", config.get("dir", "nil")]
     return "unknown command"
 
-def handle_request(connection, request_num):
-    print(f"Handling request {request_num}")
+
+def handle_request(connection):
     crlf = "\r\n"
     while True:
         data = connection.recv(1024).decode("utf-8").strip().lower()
@@ -45,29 +55,28 @@ def handle_request(connection, request_num):
         print(f"Received data: {data}")
         data_processed = process_data(data.split(crlf))
         print(f"processed data: {data_processed}")
-        
+
         response = wrap(data_processed)
 
         print(f"Response: {response}")
         connection.sendall(bytes(response, "utf-8"))
-    print(f"Request handled : {request_num}")
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_num = 0
-    while True:
-        connection, _ = server_socket.accept()
-        client_num += 1
-        thread1 = t.Thread(target=handle_request, args=(connection, client_num))
-        thread1.start()
+    print("Server started at port 6379")
+    try:
+        while True:
+            connection, _ = server_socket.accept()
+            t.Thread(target=handle_request, args=(connection,)).start()
+    finally:
+        server_socket.close()
 
-    # server_socket.close()
 
 if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--dir", type=str)
+    args.add_argument("--dbfilename", type=str)
+    args = args.parse_args()
+    config = args.__dict__
     main()
